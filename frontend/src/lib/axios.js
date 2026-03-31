b88406c17c8d2c7102874f8474f502623926d6c6
salman09-prog <salmanyusufzai48@gmail.com>
2026-04-01 01:48:12 +0530

    Initial deployment commit

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
     // It will look for VITE_API_URL in your Vercel settings or .env file
-    baseURL: "https://prerna-ecomm-1.onrender.com/api"
+    baseURL: "https://prerna-ecomm.onrender.com/api"
 })
 
 export default axiosInstance;
