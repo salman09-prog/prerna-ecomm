76465d7864332ee789d8871ea9476ecad8f463e1
salman09-prog <salmanyusufzai48@gmail.com>
2026-04-01 00:30:19 +0530

    Initial deployment commit

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,7 @@ import cartRoutes from "./routes/cart.route.js";
 import couponRoutes from "./routes/coupon.route.js";
 import paymentRoutes from "./routes/payment.route.js";
 import analyticsRoutes from "./routes/analytics.route.js";
+import cors from "cors"
 
 import { connectDB } from "./lib/db.js";
 
